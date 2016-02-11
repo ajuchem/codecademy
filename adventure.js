a59dad43bf26cb5d104fdd9815c0925b144ff80f
ajuchem <ajuchem@daudt.com>
2016-02-11 11:05:43 -0700

    Use switch/case on an adventure game

--- adventure.js
+++ adventure.js
@@ -1,4 +1,4 @@
-
+// Adventure game using switch/case 
 
 var troll = prompt("You're walking through the forest, minding your own business, and you run into a troll! Do you FIGHT him, PAY him, or RUN?").toUpperCase();
 
@@ -7,7 +7,7 @@ switch(troll) {
     var strong = prompt("How courageous! Are you strong (YES or NO)?").toUpperCase();
     var smart = prompt("Are you smart?").toUpperCase();
     if(strong === 'YES' || smart === 'YES') {
-      console.log("You only need one of the two! You beat the troll--nice work!");
+      console.log("You only need one of the two anyway! You beat the troll--nice work!");
     } else {
       console.log("You're not strong OR smart? Well, if you were smarter, you probably wouldn't have tried to fight a troll. You lose!");
     }
@@ -17,8 +17,10 @@ switch(troll) {
     var dollars = prompt("Is your money in Troll Dollars?").toUpperCase();
     if(money === 'YES' && dollars === 'YES') {
       console.log("Great! You pay the troll and continue on your merry way.");
-    } else {
+    } else if (money === 'YES' && dollars === 'NO'){
       console.log("Dang! This troll only takes Troll Dollars. You get whomped!");
+    } else {
+      console.log("Dang! You are out of luck! You get whomped!");
     }
     break;
   case 'RUN':
